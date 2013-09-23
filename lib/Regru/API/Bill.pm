0f86d9bde9cc65b0ee9f1fdf13bc8ac3f2fdd51f
Anton Gerasimov <me@zyxmasta.com>
2013-09-23 11:54:14 +0600

    updated bill category's pod file

--- lib/Regru/API/Bill.pm
+++ lib/Regru/API/Bill.pm
@@ -36,36 +36,94 @@ __END__
 
 =head1 DESCRIPTION
 
-REG.API bill category... (to be described)
+REG.API invoices management section.
 
 =method nop
 
-...
+For testing purposes. Scope: B<clients>. Typical usage:
+
+    $resp = $client->bill->nop(
+        bill_id => 12345
+    );
+
+    # or
+    $resp = $client->bill->nop(
+        bills => [ 12345, 12346 ]
+    );
+
+Returns payment status for requested invoice or error code (in some cases).
+
+More info at L<Invoice management: nop|https://www.reg.com/support/help/API-version2#bill_nop>.
 
 =method get_not_payed
 
-...
+Obtains a list of unpaid invoices. Scope: B<clients>. Typical usage:
+
+    $resp = $client->bill->get_not_payed(
+        limit => 10, offset => 40
+    );
+
+Returns a list of unpaind invoices if any.
+
+More info at L<Invoice management: get_not_payed|https://www.reg.com/support/help/API-version2#bill_get_not_payed>.
 
 =method get_for_period
 
-...
+Obtains a list of invoices for the defined period. Scope: B<partners>. Typical usage:
+
+    $resp = $client->bill->get_for_period(
+        limit      => 5,
+        start_date => '1917-10-26',
+        end_date   => '1917-10-07',
+    );
+
+Returns a list of invoices at given period if any.
+
+More info at L<Invoice management: get_for_period|https://www.reg.com/support/help/API-version2#bill_get_for_period>
 
 =method change_pay_type
 
-...
+This one allows to change payment method for selected invoice(s). Scope: B<clients>. Typical usage:
+
+    $resp = $client->bill->change_pay_type(
+        bills    => [ 12345, 12346 ],
+        pay_type => 'prepay',
+        currency => 'USD',
+    );
+
+Returns a list of invoices with payment information and status.
+
+More info at L<Invoice management: change_pay_type|https://www.reg.com/support/help/API-version2#bill_change_pay_type>
 
 =method delete
 
-...
+Allows to delete unpaid invoices. Scope: B<clients>. Typical usage:
+
+    $resp = $client->bill->delete(
+        bills => [ 12345, 12346, 12347 ]
+    );
+
+Returns a list of invoices which requested to delete and its status.
+
+More info at L<Invoice management: delete|https://www.reg.com/support/help/API-version2#bill_delete>
 
 =attr namespace
 
-...
+Always returns the name of category: C<bill>. For internal usage only.
+
+=head1 CAVEATS
+
+Bear in mind that might be errors during API requests. You should always check API call status.
+See L<Common error codes|https://www.reg.com/support/help/API-version2#std_error_codes>.
 
 =head1 SEE ALSO
 
 L<Regru::API>
 
 L<Regru::API::Role::Client>
+
+L<REG.API Invoice management|https://www.reg.com/support/help/API-version2#bill_fn>
+
+L<REG.API Common error codes|https://www.reg.com/support/help/API-version2#std_error_codes>.
 
 =cut
