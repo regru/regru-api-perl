6976037f9faf0d62e8ed43a79f7dd311bbef29e9
Anton Gerasimov <me@zyxmasta.com>
2013-10-01 03:18:12 +0600

    updated domain category docs

--- lib/Regru/API/Domain.pm
+++ lib/Regru/API/Domain.pm
@@ -232,79 +232,226 @@ More info at L<Domain management: get_docs_upload_uri|https://www.reg.com/suppor
 
 =method update_contacts
 
-... Scope: B<clients>. Typical usage:
+Make changes of the domain name contact data. Scope: B<clients>. Typical usage:
+
+    $resp = $client->domain->update_contacts(
+        domains => [
+            { dname => 'griffoth.com' },
+            { dname => 'jahoo.net' },
+        ],
+        contacts => {
+            # XXX keys of the contacts data may differ from zone to zone
+
+            # Domain name Owner contact data (o_*)
+            o_company       => 'Private person',
+            o_first_name    => 'Madame',
+            o_last_name     => 'Kovarian',
+            o_email         => 'patch-lady@kovarian.com',
+            ...
+            o_country_code  => 'US',
+            ...
+
+            # Domain name Administrative contact data (a_*)
+            ...
+
+            # Domain name Technical contact data (t_*)
+            ...
+
+            # Domain name Billing contact data (b_*)
+            ...
+
+            # Additional data
+            private_person_flag => 1,
+        }
+    );
+
+Answer will contains a domains field with list of domain names each item consist of dname, service_id and/or error_code
+parameters.
 
 More info at L<Domain management: update_contacts|https://www.reg.com/support/help/API-version2#domain_update_contacts>.
 
 =method update_private_person_flag
 
-... Scope: B<clients>. Typical usage:
+Change settings of the Private Person and Total Private Person flags (show/hide contact data in WHOIS answers).
+Scope: B<clients>. Typical usage:
+
+    $resp = $client->domain->update_private_person_flag(
+        domains => [
+            { dname => 'griffoth.com' },
+            { dname => 'jahoo.net' },
+        ],
+        private_person_flag => 1, # or 0, to expose contacts data
+    );
+
+Answer will contains a domains field with list of domain names and operation status.
 
 More info at L<Domain management: update_private_person_flag|https://www.reg.com/support/help/API-version2#domain_update_private_person_flag>.
 
 =method register_ns
 
-... Scope: B<clients>. Typical usage:
+Domain name server registration in the NSI registry ((for internatonal domains only). Scope: B<clients>. Typical usage:
+
+    $resp = $client->domain->register_ns(
+        domain_name => 'griffoth.com',
+        ns0         => 'ns1.griffoth.com',
+        ns0ip       => '172.20.21.1',
+    );
+
+Answer will contains a C<resp> field with a detailed response from the NSI registry, typically a hash. Available
+in sussessful resposnses only.
 
 More info at L<Domain management: register_ns|https://www.reg.com/support/help/API-version2#domain_register_ns>.
 
 =method delete_ns
 
-... Scope: B<clients>. Typical usage:
+Deletion of a domain name server from the NSI registry (for international domains only). Scope: B<clients>. Typical usage:
+
+    $resp = $client->domain->delete_ns(
+        domain_name => 'griffoth.com',
+        ns0         => 'ns1.griffoth.com',
+        ns0ip       => '172.20.21.1',
+    );
+
+Answer will contains a C<resp> field with a detailed response from the NSI registry, typically a hash. Available
+in sussessful resposnses only.
 
 More info at L<Domain management: delete_ns|https://www.reg.com/support/help/API-version2#domain_delete_ns>.
 
 =method get_nss
 
-... Scope: B<clients>. Typical usage:
+Retrive a domane name servers for domain name(s). Scope: B<clients>. Typical usage:
+
+    $resp = $client->domain->get_nss(
+        domain_name => 'griffoth.com',
+    );
+
+    # or
+    $resp = $client->domain->get_nss(
+        domains => [
+            { dname => 'griffoth.com' },
+            { dname => 'jahoo.net' },
+        ],
+    );
+
+Answer will contains a list of domain names and nameservers (and ip addresses if any) and/or error codes.
 
 More info at L<Domain management: get_nss|https://www.reg.com/support/help/API-version2#domain_get_nss>.
 
 =method update_nss
 
-... Scope: B<clients>. Typical usage:
+Change DNS servers of the domain name. Also this function enables/disables domain name delegation (for partners only).
+Scope: B<clients>/B<partners>. Typical usage:
+
+    $resp = $client->domain->update_nss(
+        domains => [
+            { dname => 'koorharn.ru' },
+            { dname => 'stormcage.ru' },
+        ],
+        nss => {
+            ns0 => 'ns1.barcelona.net',
+            ns1 => 'ns2.barcelona.org',
+            ns3 => 'ns3.barcelona.com',
+        },
+    );
+
+Answer will contains a list of domain names with the parameters dname, service_id and/or a service identification error_code.
 
 More info at L<Domain management: update_nss|https://www.reg.com/support/help/API-version2#domain_update_nss>.
 
 =method delegate
 
-... Scope: B<partners>. Typical usage:
+Turn on a domain name delegation flag. Scope: B<partners>. Typical usage:
+
+    $resp = $client->domain->delegate(
+        domains => [
+            { dname => 'koorharn.ru' },
+            { dname => 'stormcage.ru' },
+        ],
+    );
+
+Answer will contains a list of domain names with the parameters dname, service_id and/or a service identification error_code.
 
 More info at L<Domain management: delegate|https://www.reg.com/support/help/API-version2#domain_delegate>.
 
 =method undelegate
 
-... Scope: B<partners>. Typical usage:
+Turn off a domain name delegation flag. Scope: B<partners>. Typical usage:
+
+    $resp = $client->domain->undelegate(
+        domains => [
+            { dname => 'koorharn.ru' },
+            { dname => 'stormcage.ru' },
+        ],
+    );
+
+Answer will contains a list of domain names with the parameters dname, service_id and/or a service identification error_code.
 
 More info at L<Domain management: undelegate|https://www.reg.com/support/help/API-version2#domain_undelegate>.
 
 =method transfer_to_another_account
 
-... Scope: B<partners>. Typical usage:
+Transfer a domain name to another account within REG.RU. Scope: B<partners>. Typical usage:
+
+    $resp = $client->domain->transfer_to_another_account(
+        domain_name   => 'stormcage.ru',
+        new_user_name => 'river-song',
+    );
+
+Answer will contains a list of domain names transferred to another account. In case of success, the field
+C<result> there will be the B<request_is_sent> value for each domain name in the C<result> field, otherwise an
+error code will be returned.
 
 More info at L<Domain management: transfer_to_another_account|https://www.reg.com/support/help/API-version2#domain_transfer_to_another_account>.
 
 =method look_at_entering_list
 
-... Scope: B<partners>. Typical usage:
+Show the list of domain names transferred to current account. Scope: B<partners>. Typical usage:
+
+    $resp = $client->domain->look_at_entering_list;
+
+Answer will contains a list of messages about domain names transfer. Each message contains an ID and the name of
+the transferred domain. Upon each transfer domain names are assigned to new user ID.
 
 More info at L<Domain management: look_at_entering_list|https://www.reg.com/support/help/API-version2#domain_look_at_entering_list>.
 
 =method accept_or_refuse_entering_list
 
-... Scope: B<partners>. Typical usage:
+Accept or decline domain names transferred to current account. Scope: B<partners>. Typical usage:
+
+    $resp = $client->domain->accept_or_refuse_entering_list(
+        dname       => 'stormcage.ru',
+        id          => 895901,
+        action_type => 'accept', # accept/refuse; yes/no; 1/0
+    );
+
+Answer will contains a list of domain names with result for each domain name.
 
 More info at L<Domain management: accept_or_refuse_entering_list|https://www.reg.com/support/help/API-version2#domain_accept_or_refuse_entering_list>.
 
 =method cancel_transfer
 
-... Scope: B<partners>. Typical usage:
+Shut down transfers of the domain names. Scope: B<partners>. Typical usage:
+
+    $resp = $client->domain->cancel_transfer(
+        domains => [
+            { dname => 'koorharn.ru' },
+            { dname => 'stormcage.ru' },
+        ],
+    );
+
+Answer will contains a list of domain names with result for each domain name.
 
 More info at L<Domain management: cancel_transfer|https://www.reg.com/support/help/API-version2#domain_cancel_transfer>.
 
 =method request_to_transfer
 
-... Scope: B<partners>. Typical usage:
+Send request to transfer a domain name to foreign registrar. Scope: B<partners>. Typical usage:
+
+    $resp = $client->domain->cancel_transfer(
+        domain_name => 'felspoon.com',
+    );
+
+Answer will contains a list of domain names with result for each domain name.
 
 More info at L<Domain management: request_to_transfer|https://www.reg.com/support/help/API-version2#domain_request_to_transfer>.
 
