67e25dd3c79dbf7abcb699e2804e04116685ac99
Anton Gerasimov <me@zyxmasta.com>
2013-10-10 11:11:23 +0600

    added documentation related to dns management methods

--- lib/Regru/API/Zone.pm
+++ lib/Regru/API/Zone.pm
@@ -47,84 +47,344 @@ __END__
 
 =head1 DESCRIPTION
 
-REG.API zone category... (to be described)
+REG.API DNS management methods such as create/remove resource records, enable/disable parking and forwarding features.
 
 =apimethod nop
 
-...
+For testing purposes to check the ability to manage DNS resource records. This feature is available for domain names
+that hosted by REG.RU DNS servers only. Scope: B<clients>. Typical usage:
+
+    $resp = $client->zone->nop(
+        domains => [
+            { dname => 'bluth-company.com' },
+            { dname => 'sitwell-enterprises.com' },
+        ],
+    );
+
+Answer will contains a field C<domains> with a list of domain names which allows to manage resource records
+or error otherwise.
+
+More info at L<DNS management: nop|https://www.reg.com/support/help/API-version2#zone_nop>.
 
 =apimethod add_alias
 
-...
+Creates an A (IPv4 address) resource record for domain(s). Scope: B<clients>. Typical usage:
+
+    $resp = $client->zone->add_alias(
+        subdomain   => 'gob',
+        ipaddr      => '172.26.14.51',
+        domains     => [
+            { dname => 'bluth-company.com' },
+        ],
+    );
+
+B<NOTE> Also allowed to pass subdomain as C<@> (at) - resource record will point to domain itself or
+C<*> (asterisk) - catch-all resource record.
+
+Answer will contains a field C<domains> with a list of results for each involved to this operation domain names or
+error otherwise.
+
+More info at L<DNS management: add_alias|https://www.reg.com/support/help/API-version2#zone_add_alias>.
 
 =apimethod add_aaaa
 
-...
+Creates an AAAA (IPv6 address) resource record for domain(s). Scope: B<clients>. Typical usage:
+
+    $resp = $client->zone->add_aaaa(
+        subdomain   => 'coffee',
+        ipaddr      => '2001:0db8:11a3:09d7:1f34:8a2e:07a0:765d',
+        domains     => [
+            { dname => 'gobias-industries.net' },
+        ],
+    );
+
+This one also supports a special names for subdomains. See note for L</add_alias>.
+
+Answer will contains a field C<domains> with a list of results for each involved to this operation domain names or
+error otherwise.
+
+More info at L<DNS management: add_aaaa|https://www.reg.com/support/help/API-version2#zone_add_aaaa>.
 
 =apimethod add_cname
 
-...
+Creates a CNAME (canonical name) resource record for domain(s). Scope: B<clients>. Typical usage:
+
+    $resp = $client->zone->add_cname(
+        subdomain       => 'products',
+        canonical_name  => 'coffee.gobias-industries.net',
+        domain_name     => 'gobias.co.uk',
+    );
+
+Answer will contains a field C<domains> with a list of results for each involved to this operation domain names or
+error otherwise.
+
+More info at L<DNS management: add_cname|https://www.reg.com/support/help/API-version2#zone_add_cname>.
 
 =apimethod add_mx
 
-...
+Creates a MX (mail exchange) resource record for domain(s). Scope: B<clients>. Typical usage:
+
+    $resp = $client->zone->add_mx(
+        subdomain   => '@',
+        priority    => 5,
+        mail_server => 'mail.hot-cops.xxx',         # mail server host should have an A/AAAA record(s)
+        domains     => [
+            { dname => 'blue-man-group.org' },
+            { dname => 'gobias-industri.es' },
+            { dname => 'sudden-valley.travel' },
+        ],
+    );
+
+Answer will contains a field C<domains> with a list of results for each involved to this operation domain names or
+error otherwise.
+
+More info at L<DNS management: add_mx|https://www.reg.com/support/help/API-version2#zone_add_mx>.
 
 =apimethod add_ns
 
-...
+Creates a NS (name server) resource record which will delegate a subdomain onto the other name server. Scope: B<clients>.
+Typical usage:
+
+    $resp = $client->zone->add_ns(
+        subdomain       => 'annyong',
+        dns_server      => 'ns1.milford-school.ac.us',
+        domain_name     => 'bluth-family.ru',
+        record_number   => 1,   # just for relative arrangement of the NS records
+    );
+
+Answer will contains a field C<domains> with a list of results for each involved to this operation domain names or
+error otherwise.
+
+More info at L<DNS management: add_ns|https://www.reg.com/support/help/API-version2#zone_add_ns>.
 
 =apimethod add_txt
 
-...
+Creates a TXT (text) resource record up to 512 characters in length. Scope: B<clients>. Typical usage:
+
+    $resp = $client->zone->add_txt(
+        subdomain   => '@',
+        domain_name => 'bluth-company.com',
+        text        => 'v=spf1 include:_spf.google.com ~all',
+    );
+
+Answer will contains a field C<domains> with a list of results for each involved to this operation domain names or
+error otherwise.
+
+More info at L<DNS management: add_txt|https://www.reg.com/support/help/API-version2#zone_add_txt>.
 
 =apimethod add_srv
 
-...
+Creates a SRV (service locator) resource record. Scope: B<clients>. Typical usage:
+
+    $resp = $client->zone->add_srv(
+        domain_name => 'gobias-industri.es',
+        service     => '_sip._tcp',
+        priority    => 0,
+        weight      => 5,
+        port        => 5060,
+        target      => 'phone.gobias.co.uk',        # target host should have an A/AAAA record(s)
+    );
+
+Answer will contains a field C<domains> with a list of results for each involved to this operation domain names or
+error otherwise.
+
+More info at L<DNS management: add_srv|https://www.reg.com/support/help/API-version2#zone_add_srv>.
 
 =apimethod get_resource_records
 
-...
+Retrieves all resource records for domain(s). Scope: B<clients>. Typical usage:
+
+    $resp = $client->zone->get_resource_records(
+        domains => [
+            { dname => 'gangie.tv' },
+            { dname => 'wrench.tv' },
+        ],
+    );
+
+Answer will contains a field C<domains> with a list of domains. For each domain will be shown a resource records set (as a
+list), settings of the SOA resource record. If any error will occur this also will be reported.
+
+More info at L<DNS management: get_resource_records|https://www.reg.com/support/help/API-version2#zone_get_resource_records>.
 
 =apimethod update_records
 
-...
+Takes a set of actions and manipulates the resource records in batch mode. Scope: B<partners>. Typical usage:
+
+    $update = [
+        { action => 'add_alias',     subdomain => '@',   ipaddr => '127.0.0.1' },
+        { action => 'add_alias',     subdomain => 'www', ipaddr => '127.0.0.1' },
+        { action => 'add_mx',        subdomain => '@',   priority => 5,  mail_server => 'mx.bluth-company.net' },
+        { action => 'add_mx',        subdomain => '@',   priority => 10, mail_server => 'mx.bluth-family.com' },
+        { action => 'remove_record', subdomain => 'maeby',  record_type => 'TXT' },
+        { action => 'remove_record', subdomain => 'buster', record_type => 'A', content => '10.13.0.5' },
+        { action => 'add_txt',       subdomain => 'maeby',  text => 'Marry Me!' },
+    ];
+    $resp = $client->zone->update_records(
+        domain_name => 'bluth.com',
+        action_list => $update,
+    );
+
+    # or more complex
+    $update1 = [ # actions for 'gobias.com'
+        { action => '..', ... },
+        ...
+    ];
+    $update2 = [ # actions for 'gobias.net'
+        { action => '..', ... },
+        ...
+    ];
+    $resp = $client->zone->update_records(
+        domains => [
+            { dname => 'gobias.com', action_list => $update1 },
+            { dname => 'gobias.net', action_list => $update2 },
+        ],
+    );
+
+
+Action should one of allowed methods related to resource records: L</add_alias>, L</add_aaaa>, L</add_cname>, L</add_mx>,
+L</add_ns>, L</add_txt>, L</add_srv> or L</remove_record>.
+
+Answer will contains a field C<domains> with a list of results for each involved to this operation domain names and actions or
+error otherwise.
+
+More info at L<DNS management: update_records|https://www.reg.com/support/help/API-version2#zone_update_records>.
 
 =apimethod update_soa
 
-...
+Changes a cache settings for the SOA (start of authority) resource record. Scope: B<clients>. Typical usage:
+
+    $resp = $client->zone->update_soa(
+        ttl         => '2h', # for the entire zone
+        minimum_ttl => '1h', # for the NXDOMAIN answers
+        domains     => [
+            { dname => 'gobias.com' },
+            { dname => 'gobias.net' },
+        ],
+    );
+
+Answer will contains a field C<domains> with a list of results for each involved to this operation domain names or
+error otherwise.
+
+More info at L<DNS management: update_soa|https://www.reg.com/support/help/API-version2#zone_update_soa>.
 
 =apimethod tune_forwarding
 
-...
+Enables a web forwarding feature for domain name(s). Scope: B<clients>. Typical usage:
+
+    $resp = $client->zone->tune_forwarding(
+        domain_name => 'barrygood.biz',
+    );
+
+Prior to use this method ensure that desired domain name(s) has attached and configured correctly a C<srv_webfwd> service.
+This can be done by using methods L<Regru::API::Domain/create> or L<Regru::API::Domain/update>.
+
+Answer will contains a field C<domains> with a list of results for each involved to this operation domain names or
+error otherwise.
+
+More info at L<DNS management: tune_forwarding|https://www.reg.com/support/help/API-version2#zone_tune_forwarding>.
 
 =apimethod clear_forwarding
 
-...
+Disables a web forwarding feature for domain name(s). Scope: B<clients>. Typical usage:
+
+    $resp = $client->zone->clear_forwarding(
+        domain_name => 'barrygood.biz',
+    );
+
+Prior to use this method ensure that desired domain name(s) has attached and configured correctly a C<srv_webfwd> service.
+This can be done by using methods L<Regru::API::Domain/create> or L<Regru::API::Domain/update>.
+
+Answer will contains a field C<domains> with a list of results for each involved to this operation domain names or
+error otherwise.
+
+More info at L<DNS management: clear_forwarding|https://www.reg.com/support/help/API-version2#zone_clear_forwarding>.
 
 =apimethod tune_parking
 
-...
+Enables a web parking feature for domain name(s). Scope: B<clients>. Typical usage:
+
+    $resp = $client->zone->tune_parking(
+        domains => [
+            { dname => 'barrygood.biz' },
+        ],
+    );
+
+Prior to use this method ensure that desired domain name(s) has attached and configured correctly a C<srv_parking> service.
+This can be done by using methods L<Regru::API::Domain/create> or L<Regru::API::Domain/update>.
+
+Answer will contains a field C<domains> with a list of results for each involved to this operation domain names or
+error otherwise.
+
+More info at L<DNS management: tune_parking|https://www.reg.com/support/help/API-version2#zone_tune_parking>.
 
 =apimethod clear_parking
 
-...
+Disables a web parking feature for domain name(s). Scope: B<clients>. Typical usage:
+
+    $resp = $client->zone->clear_parking(
+        domains => [
+            { dname => 'barrygood.biz' },
+        ],
+    );
+
+Prior to use this method ensure that desired domain name(s) has attached and configured correctly a C<srv_parking> service.
+This can be done by using methods L<Regru::API::Domain/create> or L<Regru::API::Domain/update>.
+
+Answer will contains a field C<domains> with a list of results for each involved to this operation domain names or
+error otherwise.
+
+More info at L<DNS management: clear_parking|https://www.reg.com/support/help/API-version2#zone_clear_parking>.
 
 =apimethod remove_record
 
-...
+Removes any resource record from domain name(s). Scope: B<clients>. Typical usage:
+
+    $resp = $client->zone->remove_record(
+        domains => [
+            { dname => 'cia.com' },
+        ],
+        subdomain   => 'tobias',
+        record_type => 'TXT',
+        content     => 'Mr. F!',
+    );
+
+Answer will contains a field C<domains> with a list of results for each involved to this operation domain names or
+error otherwise.
+
+More info at L<DNS management: remove_record|https://www.reg.com/support/help/API-version2#zone_remove_record>.
 
 =apimethod clear
 
-...
+B<Watch out! Handy way to get lost everything!>
+
+Deletes ALL resource records. Scope: B<clients>. Typical usage:
+
+    $resp = $client->zone->clear(
+        domains => [
+            { dname => 'scandalmakers.com' },
+            { dname => 'weathers.net' },
+        ],
+    );
+
+Answer will contains a field C<domains> with a list of results for each involved to this operation domain names or
+error otherwise.
+
+More info at L<DNS management: clear|https://www.reg.com/support/help/API-version2#zone_clear>.
 
 =attr namespace
 
-...
+Always returns the name of category: C<zone>. For internal uses only.
 
 =head1 SEE ALSO
 
 L<Regru::API>
 
 L<Regru::API::Role::Client>
+
+L<Regru::API::Domain>
+
+L<REG.API DNS management|https://www.reg.com/support/help/API-version2#zone_fn>
+
+L<REG.API Common error codes|https://www.reg.com/support/help/API-version2#std_error_codes>.
 
 =cut
